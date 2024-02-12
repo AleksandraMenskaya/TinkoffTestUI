12224731313c8e32a5900c406eb028f6d48f8e05
Юлия Вилкова <al.menskaya@gmail.com>
2024-02-12 21:14:29 +0100

    finish_test3

--- src/test/java/pages/HelpPage.java
+++ src/test/java/pages/HelpPage.java
@@ -43,7 +43,7 @@ public class HelpPage {
 
     public HelpPage scrollToTitle (){
         titleName.scrollIntoView(true);
-        executeJavaScript("scrollBy(0, 100)"); //скролл для видимости кнопки
+        executeJavaScript("scrollBy(0, -100)"); //скролл для видимости кнопки
         return this;
     }
 
